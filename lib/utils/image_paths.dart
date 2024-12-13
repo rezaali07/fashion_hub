c259e537c4972cc4c60f1fb36aa8ae31fa479bfe
rezaali2290 <rezaali20ab@gmail.com>
2024-12-13 12:21:59 +0545

    utils

--- lib/utils/image_paths.dart
+++
@@ -1,8 +0,0 @@
-class AppImagePath {
-  static const String kRectangleBackgound = 'assets/fashionhub/background.png';
-}
-
-class LogoPath {
-  static const String kRectangleBackgound =
-      'assets/images/Fashion_Hub_logo.png';
-}
