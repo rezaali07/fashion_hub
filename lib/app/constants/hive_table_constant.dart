9cef738e885d29b52cb3e5c1028b2e9c4f4b360a
rezaali2290 <rezaali20ab@gmail.com>
2025-03-04 23:59:20 +0545

    hive table

--- lib/app/constants/hive_table_constant.dart
+++ lib/app/constants/hive_table_constant.dart
@@ -4,4 +4,10 @@ class HiveTableConstant {
   static const int userTableId = 0;
   static const String userBox = 'userBox';
 
+  static const int productTableId = 1;
+  static const String productBox = 'productBox';
+
+  static const int orderTableId = 2;
+  static const String orderBox = 'orderBox';
+
 }
