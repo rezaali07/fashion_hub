b66f2d71f138debe834914bf9e10d7ead39b6d24
rezaali2290 <rezaali20ab@gmail.com>
2025-03-06 01:13:35 +0545

    get all order

---
+++ lib/features/order/domain/use_case/get_all_order_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:fashion_hub/app/usecase/usecase.dart';
+import 'package:fashion_hub/core/error/failure.dart';
+import 'package:fashion_hub/features/Order/domain/entity/order_entity.dart';
+import 'package:fashion_hub/features/order/domain/repository/order_repository.dart';
+
+class GetAllOrderUsecase implements UsecaseWithoutParams<List<OrderEntity>> {
+  final IOrderRepository _orderRepository;
+
+  GetAllOrderUsecase({required IOrderRepository orderRepository})
+      : _orderRepository = orderRepository;
+
+  @override
+  Future<Either<Failure, List<OrderEntity>>> call() {
+    return _orderRepository.getOrder();
+  }
+
+  // @override
+  // Future<Either<Failure, List<OrderEntity>>> call() {
+  //   return _orderRepository.getOrder();
+  // }
+}
