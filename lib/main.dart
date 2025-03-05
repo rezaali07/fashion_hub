e66dfa4209e3324530ea9a4e11fac3ac2d846c68
rezaali2290 <rezaali20ab@gmail.com>
2025-03-06 00:56:03 +0545

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:fashion_hub/app/app.dart';
 import 'package:fashion_hub/app/di/di.dart';
-import 'package:flutter/cupertino.dart';
-
-import 'core/network/hive_service.dart';
+import 'package:fashion_hub/core/network/hive_service.dart';
+import 'package:fashion_hub/features/Product/data/model/product_hive_model.dart';
+import 'package:fashion_hub/features/order/data/model/order_hive_model.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Register Hive adapters
+  Hive.registerAdapter(ProductHiveModelAdapter());
+  Hive.registerAdapter(OrderHiveModelAdapter());
 
   // Initialize Hive Database
   await HiveService().init();
 
+  // Initialize Dependencies (Dependency Injection)
   await initDependencies();
-  runApp(
-    App(),
-  );
-}
 
-// First create Entity => Model => Repository
+  // Run the main application
+  runApp(App());
+}
